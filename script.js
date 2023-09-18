30e0178444a8bf1b8620ad1e0e412a32b92a285a
“Ygor <igorgarofalo@gmail.com>
2023-09-18 02:57:25 +0200

    added form with card

--- script.js
+++ script.js
@@ -11,8 +11,20 @@ const createdivs = function () {
     table.appendChild(divs);
   }
 };
-
 createdivs();
+
+const createcards = function () {
+  for (let i = 1; i <= 20; i++) {
+    const cardSections = document.getElementById("card-sections");
+
+    const cards = document.createElement("div");
+    cards.innerText = Math.floor(Math.random() * 90) + 1;
+
+    cardSections.appendChild(cards);
+    form.remove();
+    cardSections.style.display = "inline-block";
+  }
+};
 
 const allNumbers = [];
 
@@ -23,7 +35,6 @@ const randomNumbers = function () {
     random = Math.floor(Math.random() * 91) + 1;
   } while (allNumbers.includes(random));
   allNumbers.push(random);
-
   const extractedNumber = document.querySelectorAll("#maintable div");
 
   for (let i = 0; i < extractedNumber.length; i++) {
@@ -35,7 +46,21 @@ const randomNumbers = function () {
       numbers.innerText = random;
     }
   }
+
+  const cardsNumbers = document.querySelectorAll("#card-sections div");
+  for (let x = 0; x < cardsNumbers.length; x++) {
+    const numbersOut2 = parseInt(cardsNumbers[x].innerText);
+    if (random === numbersOut2) {
+      cardsNumbers[x].classList.add("colorBackg");
+    }
+  }
 };
+
+const form = document.getElementById("form");
+form.addEventListener("submit", createcards);
+
+const input = document.getElementById("input");
+console.log(input.value);
 
 const button = document.getElementById("get-number");
 button.addEventListener("click", randomNumbers);
